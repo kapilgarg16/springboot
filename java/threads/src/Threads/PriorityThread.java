1766ac22d2f71246f4c280eb935900344cd1b631
Kapil Garg <55026503+kapilgarg16@users.noreply.github.com>
2024-04-05 04:00:01 +0530

    Add files via upload

---
+++ java/threads/src/Threads/PriorityThread.java
@@ -0,0 +1,15 @@
+package Threads;
+public class PriorityThread implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+            try {
+                Thread.sleep(500); // Sleep for 500 milliseconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
